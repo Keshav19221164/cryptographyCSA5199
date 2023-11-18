66a4e78575671eda33ec1202e59a98fdd202b4fa
Keshav19221164 <132119651+Keshav19221164@users.noreply.github.com>
2023-11-18 14:09:26 +0530

    Add files via upload

---
+++ 2.monoalphabetic.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char string[30];int k;
+	printf("enter the plain text:");
+	gets(string);
+	printf("enter the key value :");
+	scanf("%d",&k);
+	int l=strlen(string);
+	int i;
+	for(i=0;string[i]!='\0';i++)
+{
+	string[i] +=k;
+}
+printf(" the cipher text is :");
+for(i=0;string[i]!='\0';i++)
+{
+	printf("%c",string[i]);
+}
+}
